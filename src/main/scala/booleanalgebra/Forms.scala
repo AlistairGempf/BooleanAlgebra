e8c8ace8496b67c1f685732b7c361cb15db9f05e
Alistair Gempf <alistair.gempf@bbc.co.uk>
2020-05-28 09:23:17 +0100

    Add comment

--- src/main/scala/booleanalgebra/Forms.scala
+++ src/main/scala/booleanalgebra/Forms.scala
@@ -40,28 +40,39 @@ case object DNF extends NormalForm {
 
   override def and(lhs: Condition, rhs: Condition): Condition = {
     (lhs, rhs) match {
-      case (_, TrueCondition) => TrueCondition
-      case (TrueCondition, _) => TrueCondition
-      case (a, FalseCondition) => a
-      case (FalseCondition, a) => a
+        // A & 0 = 0
+      case (_, FalseCondition) => FalseCondition
+      case (FalseCondition, _) => FalseCondition
+        // A & 1 = A
+      case (a, TrueCondition) => a
+      case (TrueCondition, a) => a
+        // (a || b) && (c && d) = (a && c && d) || (b && c && d)
       case (OR(orConditions), and: AND) => OR(orConditions.map(_ && and))
       case (and: AND, OR(orConditions)) => OR(orConditions.map(and && _))
+        // (a || b) && (c || d) = (a && c) || (a && d) || (b && c) || (b && d)
       case (OR(conditionsL), OR(conditionsR)) => OR(conditionsL.flatMap(l => conditionsR.map(l && _)))
+        // a && (b || c) = (a && b) || (a && c)
       case(literal, OR(orConditions)) => OR(orConditions.map(literal && _))
       case(OR(orConditions), literal) => OR(orConditions.map(_ && literal))
+        // Default
       case (a, b) => a && b
     }
   }
 
   override def or(lhs: Condition, rhs: Condition): Condition = {
     (lhs, rhs) match {
-      case (_, FalseCondition) => FalseCondition
-      case (FalseCondition, _) => FalseCondition
-      case (a, TrueCondition) => a
-      case (TrueCondition, a) => a
+        // A | 1 = 1
+      case (_, TrueCondition) => TrueCondition
+      case (TrueCondition, _) => TrueCondition
+        // A | 0 = A
+      case (a, FalseCondition) => a
+      case (FalseCondition, a) => a
+        // (a || b) || (c || d) = a || b || c || d
       case (OR(conditionsL), OR(conditionsR)) => OR(conditionsL ++ conditionsR)
+        // (a && b) || (c || d) = (a && b) || c || d
       case (and: AND, OR(orConditions)) => OR(orConditions + and)
       case (OR(orConditions), and: AND) => OR(orConditions + and)
+        // default
       case (a, b) => a || b
     }
   }
